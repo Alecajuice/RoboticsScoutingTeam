2e7150130b35ba363a71dfb808cd4bc5d60949da
Alex Tarng <alecadad@gmail.com>
2014-02-02 16:37:23 -0800

    asdf

---
+++ Scouting Program/src/Main.java
@@ -0,0 +1,81 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main extends JPanel implements KeyListener, MouseListener, MouseMotionListener
+{
+	private static String windowName = "Scouter";
+    static int screenWidth = 500;
+    static int screenHeight = 500;
+	public static void main(String[] args) throws IOException
+	{
+        JFrame frame = new JFrame();
+        frame.setSize(screenWidth, screenHeight);
+        frame.setTitle(windowName);
+        frame.setLocationRelativeTo(null);
+        frame.setBackground(Color.WHITE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Main drawer = new Main();
+        frame.add(drawer);
+        frame.setVisible(true);
+        frame.addKeyListener(drawer);
+        frame.addMouseListener(drawer);
+        frame.addMouseMotionListener(drawer);
+        Robot robot = new Robot(1);
+        for(Object s : robot.scanMobility())
+        {
+        	System.out.println(s);
+        }
+	}
+	
+	//Mouse events
+	public void mouseDragged(MouseEvent arg0) 
+	{
+		
+	}
+	public void mouseMoved(MouseEvent arg0) 
+	{
+		
+	}
+	public void mouseClicked(MouseEvent arg0) 
+	{
+		
+	}
+	public void mouseEntered(MouseEvent arg0) 
+	{
+		
+	}
+	public void mouseExited(MouseEvent arg0) 
+	{
+		
+	}
+	public void mousePressed(MouseEvent arg0) 
+	{
+		
+	}
+	public void mouseReleased(MouseEvent arg0) 
+	{
+		
+	}
+	
+	//Keyboard Events
+	public void keyPressed(KeyEvent arg0) 
+	{
+		
+	}
+	public void keyReleased(KeyEvent arg0) 
+	{
+		
+	}
+	public void keyTyped(KeyEvent arg0) 
+	{
+		
+	}
+}
